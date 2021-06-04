7c0424d1986b3a79b03aa3b0fc6757772c8bc04b
Lam Nhan <lamnhan92@gmail.com>
2021-06-04 17:16:00 +0700

    'deploy:app'

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  gettingStartedContent =
+`Install the CLI:
+\`\`\`sh
+npm install -g @molacms/cli
+\`\`\`
+
+Create a new project:
+\`\`\`sh
+mola new blank myApp
+\`\`\`
+
+See [@molacms/cli](https://github.com/themolacms/cli) for detail usage.`;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkDirectiveModule } from '@lamnhan/ngx-useful';
+import { NguixContentComponentModule } from '@lamnhan/nguix-starter';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -13,6 +14,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     RouterLinkDirectiveModule,
+    NguixContentComponentModule,
     HomeRoutingModule
   ]
 })
